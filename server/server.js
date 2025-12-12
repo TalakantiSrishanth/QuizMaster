e91eaee7e1879587d9b8a336f19de2427d7089ae
Srishanth <talakantisrishanth1629@gmail.com>
2025-12-12 13:14:39 +0530

    Modified server.js

--- server/server.js
+++ server/server.js
@@ -154,4 +154,4 @@ app.post("/api/finish", async (req, res) => {
 
 
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("Server running on port 5000"));
